8b76cf1be1e934ff9352e668faf613aba34bc489
reddysruthi <116589336+reddysruthi@users.noreply.github.com>
2022-10-29 21:49:27 +0530

    updated a nginx-install file

--- install-nginx.sh
+++ install-nginx.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install -y nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
-echo "<h1>nginx was deployed success by using terraform $(hostname -f)</h1>" > /usr/share/nginx/html/index.html
-echo "<h1>Nginx was deployed success by using terraform $(hostname -f)</h1>" > /var/www/html/index.html
+echo "<h1>nginx was deployed success by using jenkins $(hostname -f)</h1>" > /usr/share/nginx/html/index.html
+echo "<h1>Nginx was deployed success by using jenkins $(hostname -f)</h1>" > /var/www/html/index.html
